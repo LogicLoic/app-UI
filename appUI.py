16bfd3a3bae7191c52d523ef32a0fe19a5ac8525
Loïc <logicloic08@gmail.com>
2025-10-13 11:55:03 +0200

    Adding pointing function

--- appUI.py
+++ appUI.py
@@ -8,6 +8,14 @@ class Object:
         self.id = canvas.create_rectangle(x*WIDTH+50, y*HEIGHT+50, x*WIDTH+WIDTH/AMOUNT_PER_LINE*0.9, y*HEIGHT+HEIGHT/AMOUNT_PER_LINE*0.9,
                                           fill="#0066bb", outline="#0088ff", width=2)
 
+    def pointed(self, event):
+        coords = self.canvas.coords(self.id)
+        while coords[0] <= event.x <= coords[2] and coords[1] <= event.y <= coords[3]:
+            self.canvas.itemconfig(self.id, fill="#00bb00")
+            self.canvas.update()
+        self.canvas.itemconfig(self.id, fill="#0066bb")
+        self.canvas.update()
+
 root = Tk()
 
 WIDTH = root.winfo_screenwidth()
@@ -27,5 +35,6 @@ objects = []
 
 for i in range(10):
     objects.append(Object(canvas, (i%AMOUNT_PER_LINE)*(1/AMOUNT_PER_LINE), (i//AMOUNT_PER_LINE)*(1/AMOUNT_PER_LINE)))
+    canvas.tag_bind(objects[i].id, "<Enter>", objects[i].pointed)
 
 root.mainloop()
